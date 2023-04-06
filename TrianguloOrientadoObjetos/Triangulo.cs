8cc6f2f38457a9589e402d53c3078168d053fa62
GabrielCMWerner <gabrielcmw1094@gmail.com>
2023-04-06 13:48:55 -0300

    Add project files.

---
+++ TrianguloOrientadoObjetos/Triangulo.cs
@@ -0,0 +1,32 @@
+﻿namespace TrianguloOrientadoObjetos
+{
+    public class Triangulo
+    {
+        public double Lado1;
+        public double Lado2;
+        public double Lado3;
+
+        public void DefinirTipo()
+        {
+            if (Lado1 < Lado2 + Lado3 && Lado2 < Lado1 + Lado3 && Lado3 < Lado1 + Lado2)
+            {
+                if (Lado1 == Lado2 && Lado1 == Lado3)
+                {
+                    Console.WriteLine("Este triangulo é equilátero");
+                }
+                else if (Lado1 != Lado2 && Lado1 != Lado3 && Lado2 != Lado3)
+                {
+                    Console.WriteLine("Este triangulo é escaleno");
+                }
+                else
+                {
+                    Console.WriteLine("Este triangulo é um Isósceles");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não é um Triangulo");
+            }
+        }
+    }
+}
